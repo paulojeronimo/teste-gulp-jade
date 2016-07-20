98651c710c9d28262a4620ab98d0487f40bc8d2f
Paulo Jerônimo <paulojeronimo@gmail.com>
2016-07-20 18:54:31 -0300

    Modificação do gulpfile.js para suporte a cópia de assets

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,19 @@ var gulp = require('gulp'),
     jade = require('gulp-jade');
 
 gulp.task('jade', function() {
-    return gulp
+    gulp
         .src('src/templates/**/*.jade')
-        .pipe(jade())
+        .pipe(jade({ "pretty": true }))
         .pipe(gulp.dest('build/development'));
 });
+
+gulp.task('copy-assets', function() {
+    gulp.src('bower_components/bootstrap/dist/css/bootstrap.min.css')
+        .pipe(gulp.dest('build/development/css'));
+    gulp.src('bower_components/bootstrap/dist/js/bootstrap.min.js')
+        .pipe(gulp.dest('build/development/js'));
+    gulp.src('bower_components/jquery/dist/jquery.min.js')
+        .pipe(gulp.dest('build/development/js'));
+});
+
+gulp.task('default', ['jade', 'copy-assets']);
